19373231a0e667befb4a3d2a365644dd1b15a73d
Rishabh Dubey <rishabhdubey141@gmail.com>
2021-09-27 11:49:55 +0530

    Created the final version 1.0 of the game

--- script.js
+++ script.js
@@ -1,36 +1,79 @@
-var arr = [];
-    var secret = Math.round(Math.random() * 10);
+const arr = [];
+var secret = Math.round(Math.random() * 10) + 1;
 
-    document.addEventListener("keypress", function (event) {
-      if (event.key == "Enter") {
-        event.preventDefault();
-        play();
-      }
-    });
+document.addEventListener("keypress", function (event) {
+  if (event.key == "Enter") {
+    event.preventDefault();
+    play();
+  }
+  //bug
+  /*else {
+    document.getElementById("number").value = event.key;
+  }
+  */
+});
+function reset() {
+  document.getElementById("number").value = "";
+  secret = Math.round(Math.random() * 10) + 1;
+  var ans = document.getElementById("ans");
+  ans.style.visibility = "hidden";
+  ans.style.position = "absolute";
+  arr.length = 0;
+  document.getElementById("rec").innerHTML = "";
+}
 
-    function showHistory() {
-      //   var last = document.getElementById("his");
-      //   his.innerText = "Your last text was:" + arr[arr.length - 1];
-      var e = "<hr/>";
+document.getElementById("reset").addEventListener("click", reset());
+// document.getElementById("number").addEventListener("click", function (event) {
+//   event.preventDefault();
+// });
 
-      for (var y = arr.length - 1; y >= 0; y--) {
-        e += "Last Number Entered = " + arr[y] + "<br/>";
-      }
-      document.getElementById("rec").innerHTML = e;
-    }
+function showHistory() {
+  var data = "";
 
-    function play() {
-      //   var num = document.getElementById("num").value;
-      var number = document.getElementById("number").value;
-      var ans = document.getElementById("ans");
-      if (number < secret) {
-        ans.innerText = "You gussed too low";
-      } else if (number > secret) {
-        ans.innerText = "You gussed to high";
-      } else {
-        ans.innerText = "Aweasome";
-        secret = Math.round(Math.random() * 10);
-      }
-      arr.push(number);
-      showHistory();
-    }
+  for (var y = arr.length - 1; y >= 0; y--) {
+    data += '<p class="guess">You guessed ' + arr[y] + "<p>";
+  }
+  document.getElementById("rec").innerHTML = data;
+}
+
+function addToArray(num) {
+  if (arr.length < 3) {
+    arr.push(num);
+  } else {
+    arr.shift();
+    arr.push(num);
+  }
+}
+
+function play() {
+  //   var num = document.getElementById("num").value;
+  var number = document.getElementById("number").value;
+  if (!Number(number)) {
+    reset();
+    return;
+  }
+  var ans = document.getElementById("ans");
+  ans.style.visibility = "visible";
+  ans.style.position = "relative";
+
+  if (Number(number)) {
+    addToArray(number);
+  }
+  ans.classList.remove("alert-warning");
+
+  ans.classList.remove("alert-success");
+  ans.classList.remove("alert-danger");
+  if (number < secret) {
+    ans.innerText = "You gussed too low";
+    ans.classList.add("alert-danger");
+  } else if (number > secret) {
+    ans.innerText = "You gussed to high";
+    ans.classList.add("alert-warning");
+  } else {
+    ans.innerText = "Aweasome";
+    ans.classList.add("alert-success");
+  }
+
+  showHistory();
+  document.getElementById("number").value = "";
+}
